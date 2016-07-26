e5554f063cd059535c90a149284de19d0ec4376a
Paul Talbot <paul@fullfrontalingenuity.com>
2016-07-26 19:44:01 -0400

    added _keysdir

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -21,7 +21,7 @@ class profile::master {
       'common',
     ],
 	eyaml      => true,
-	keysdir    => '/etc/puppetlabs/code-staging/keys',
+	_keysdir   => '/etc/puppetlabs/code-staging/keys',
     hiera_yaml => $hiera_yaml,
     datadir    => '/etc/puppetlabs/code/environments/%{::environment}/hieradata',
     owner      => 'pe-puppet',
